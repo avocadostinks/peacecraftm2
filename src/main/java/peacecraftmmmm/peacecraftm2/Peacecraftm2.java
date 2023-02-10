34fd5facbadcdcdae2a432202e515771627031a7
BuildTools <unconfigured@null.spigotmc.org>
2023-02-09 20:19:32 -0500

    this took so long bro I think I fixed the deprecated warnings

--- src/main/java/peacecraftmmmm/peacecraftm2/Peacecraftm2.java
+++ src/main/java/peacecraftmmmm/peacecraftm2/Peacecraftm2.java
@@ -1,18 +1,32 @@
 package peacecraftmmmm.peacecraftm2;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class Peacecraftm2 extends JavaPlugin {
+public final class Peacecraftm2 extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-        getLogger().info("my plugin has been enabled!");
+        System.out.println("thomas's plugin is now turning on hello world");
+        getServer().getPluginManager().registerEvents(this,this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-        getLogger().info("my plugin has been disabled!");
+        System.out.println("thomas's plugin is now turning off goodbye");
     }
+    @EventHandler
+    public void joinMessage(PlayerJoinEvent event) {
+
+        if (event.getPlayer().toString() == "Itsbobthebuilder") {
+            getServer().getOnlinePlayers().forEach(player -> player.sendMessage(event.getPlayer().getName() + " our god, has joined the game."));
+        } else {
+            getServer().getOnlinePlayers().forEach(player -> player.sendMessage(event.getPlayer().getName() + " has joined the game."));
+        }
+    }
+
 }
